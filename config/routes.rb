b9660c6f71b2d523a6ac58e4a8570fff5c378760
Erik Nomitch <enomitch@gmail.com>
2014-02-11 19:54:50 -0600

    better flash

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ SocialNews::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'landings#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
